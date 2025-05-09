b245b82edf940e6f83aa3725ad5689e7f0476778
GABRIEL DE LIMA CARNIATTO <gabriel.carniatto@etec.sp.gov.br>
2025-05-09 11:54:34 -0300

    vou tentar fazer login em 15 min

---
+++ MeuAppDeNavegacao/screens/DetailsScreen.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { View, Text, Button, StyleSheet, Dimensions } from 'react-native';
+
+const windowWidth = Dimensions.get('window').width;
+
+export default function DetailsScreen({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Details Screen</Text>
+      <View style={styles.buttonContainer}>
+        <Button
+          title="Go to Home"
+          onPress={() => navigation.navigate('Home')}
+        />
+      </View>
+      <View style={styles.buttonContainer}>
+        <Button
+          title="Go to Profile"
+          onPress={() => navigation.navigate('Profile')}
+        />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  buttonContainer: {
+    backgroundColor: '#f0f0f0',
+    margin: 10,
+    width: windowWidth * 0.5,
+    borderRadius: 10,
+  },
+});
---
+++ MeuAppDeNavegacao/screens/HomeScreen.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { View, Text, Button, StyleSheet, Dimensions } from 'react-native';
+
+const windowWidth = Dimensions.get('window').width;
+
+export default function HomeScreen({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Home Screen</Text>
+      <View style={styles.buttonContainer}>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Details')}
+      />
+      </View>
+      <View style={styles.buttonContainer}>
+        <Button
+        title="Go to Profile"
+        onPress={() => navigation.navigate('Profile')}
+        />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#fff',
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginBottom: 20,
+    },
+    buttonContainer: {
+        backgroundColor: '#f0f0f0',
+        margin: 10,
+        width: windowWidth * 0.5,
+        borderRadius: 10,
+    },
+});
